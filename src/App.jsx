942a99ac2e6616cdee63a576730da8fae79b3def
Sumaiya Begum <sumaiyabegum43@gmail.com>
2025-09-11 19:06:51 -0700

    text scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./sections/HeroSection";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <div className="">
       <Navbar />
       <HeroSection />
+      <div className="h-dvh border border-amber-600"></div>
     </div>
   );
 };
