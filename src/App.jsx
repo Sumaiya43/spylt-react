fc483247a193997102e00d7ed03e2168e577c7a4
Sumaiya Begum <sumaiyabegum43@gmail.com>
2025-09-13 17:06:13 -0700

    flavor section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./sections/HeroSection";
-import { ScrollTrigger } from "gsap/all";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
 import MessageSection from "./sections/messageSection";
 import FlavorSection from "./sections/FlavorSection";
+import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
   return (
     <div className="">
       <Navbar />
-      <HeroSection />
-      <MessageSection/>
-      <FlavorSection/>
-      <div className="h-dvh border border-amber-600"></div>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />
+          <div className="h-dvh border border-amber-600"></div>
+        </div>
+      </div>
     </div>
   );
 };
